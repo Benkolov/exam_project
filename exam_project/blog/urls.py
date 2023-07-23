adeeca1ad84294cd6dfb812b9bf09afb3db2032e
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-07-23 23:27:31 +0300

    Add

--- exam_project/blog/urls.py
+++ exam_project/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home_page, name='home'),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('category/<int:category_id>/', posts_by_category, name='posts_by_category'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
